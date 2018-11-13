33f95585b89bd3b08c5cd17c3df01c255e856f44
chenshenhai <cshenhai@gmail.com>
2018-11-13 23:42:53 +0800

    feat: update chapter

--- demo/chapter-01-01/src/index.js
+++ demo/chapter-01-01/src/index.js
@@ -1,14 +1,18 @@
-const Koa = require('koa');
-const graphqlHTTP = require('koa-graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 
-const app = new Koa();
+const app = express();
 const port = 3000;
 
-app.use(graphqlHTTP({
-  schema: schema,
-  graphiql: true
+app.use('/graphql', graphqlHTTP(request => {
+  return {
+    schema: schema,
+    context: { startTime: Date.now() },
+    graphiql: true,
+  };
 }));
+
 app.listen(port, function() {
   console.log(`this server is starting at port=${port}`);
 });
--- demo/chapter-01-02/src/index.js
+++ demo/chapter-01-02/src/index.js
@@ -1,18 +1,14 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const Koa = require('koa');
+const graphqlHTTP = require('koa-graphql');
 const schema = require('./schema');
 
-const app = express();
+const app = new Koa();
 const port = 3000;
 
-app.use('/graphql', graphqlHTTP(request => {
-  return {
-    schema: schema,
-    context: { startTime: Date.now() },
-    graphiql: true,
-  };
+app.use(graphqlHTTP({
+  schema: schema,
+  graphiql: true
 }));
-
 app.listen(port, function() {
   console.log(`this server is starting at port=${port}`);
 });
